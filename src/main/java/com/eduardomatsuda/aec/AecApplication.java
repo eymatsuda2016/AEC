6c29c10ba0e7e61d338e6f636570be30c26b256c
eymatsuda2016 <eymatsuda2016@outlook.com>
2024-05-28 18:50:12 -0300

    Feito as criações automáticas no banco de dados das Categorias

--- src/main/java/com/eduardomatsuda/aec/AecApplication.java
+++ src/main/java/com/eduardomatsuda/aec/AecApplication.java
@@ -1,13 +1,34 @@
 package com.eduardomatsuda.aec;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class AecApplication {
+import com.eduardomatsuda.aec.domain.Categoria;
+import com.eduardomatsuda.aec.repositories.CategoriaRepository;
 
+import com.eduardomatsuda.aec.repositories.CategoriaRepository;
+
+@SpringBootApplication
+public class AecApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(AecApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Automóveis e Caminhonetas");
+		Categoria cat2 = new Categoria(null, "Caminhões e Onibus");
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
